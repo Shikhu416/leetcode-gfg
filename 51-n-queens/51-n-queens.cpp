84809e78fea23d8845f22300abc34242b7699af2
Shikhu416 <91547324+Shikhu416@users.noreply.github.com>
2022-07-31 15:46:07 +0530

    Time: 101 ms (8.92%), Space: 45.7 MB (13.27%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isSafe1(int row, int col, vector < string > board, int n) {
+      // check upper element
+      int duprow = row;
+      int dupcol = col;
+
+      while (row >= 0 && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row--;
+        col--;
+      }
+
+      col = dupcol;
+      row = duprow;
+      while (col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        col--;
+      }
+
+      row = duprow;
+      col = dupcol;
+      while (row < n && col >= 0) {
+        if (board[row][col] == 'Q')
+          return false;
+        row++;
+        col--;
+      }
+      return true;
+    }
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (isSafe1(row, col, board, n)) {
+          board[row][col] = 'Q';
+          solve(col + 1, board, ans, n);
+          board[row][col] = '.';
+        }
+      }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>shikhar;
+        string s(n,'.');
+        vector<string>boards(n,s);
+        solve(0,boards,shikhar,n);
+        return shikhar;
+    }
+};
