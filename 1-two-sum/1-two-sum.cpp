a6a09c5a285256be4fefde87dfaed4e6d402eac1
Shikhu416 <91547324+Shikhu416@users.noreply.github.com>
2022-08-14 17:25:26 +0530

    Time: 12 ms (88.86%), Space: 10.9 MB (47.70%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       vector<int>temp;
-        
-        temp=nums;
-        vector<int>shikhar;
-        sort(temp.begin(),temp.end());
-       int first,second;
-        int l=0;
-        int r=nums.size()-1;
-        while(l<r)
-              {
-                  if(temp[l]+temp[r]==target)
-                  {
-                      first=temp[l];
-                      second=temp[r];
-                      break;
-                  }
-                 else if(temp[l]+temp[r]<target)
-                {
-                  l++;
-                 }
-               else if(temp[l]+temp[r]>target)
-              {
-                r--;
-               }
-            
-              }
-       
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==first)
-            {
-                shikhar.emplace_back(i);
-                
-            }
-            else if(nums[i]==second)
-            {
-                shikhar.emplace_back(i);
-                
-            }
-        }
+       vector<int>shikhar;
+       unordered_map<int,int>mp;
+       for(int i=0;i<nums.size();i++)
+       {
+           if(mp.find(target-nums[i])!=mp.end())
+           {
+               shikhar.push_back(i);
+               shikhar.push_back(mp[target-nums[i]]);
+               return shikhar;
+           }
+           else
+           {
+               mp[nums[i]]=i;
+           }
+       }
         return shikhar;
+      
     }
 };
