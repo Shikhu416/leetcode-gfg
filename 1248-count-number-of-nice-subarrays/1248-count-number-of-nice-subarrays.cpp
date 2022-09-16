77ca3ac398c6883dde2299e135637a27274fa9b2
Shikhu416 <91547324+Shikhu416@users.noreply.github.com>
2022-09-16 21:58:34 +0530

    Time: 567 ms (8.78%), Space: 102.3 MB (10.85%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,56 +1,19 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& v, int k) {
-        int n=v.size();
-        int shikhar=0;
-        
-       int b=0;int a=0,cnt=0;
-       while(a<n&&b<n)
-       {//bool flag=true;
-           if(v[a]%2!=0)
-           {
+        unordered_map<int,int>mp;
+        int left=0;int right=0,cnt=0,n=v.size(),ans=0;
+        while(right<n)
+        {
+           if(v[right]%2!=0)
                cnt++;
-               if(cnt==k)
-               {
-                   a++;
-                    //flag=false;
-                   int temp1=0;
-                   while(a<n&&v[a]%2==0)
-                   {
-                       
-                           temp1++;
-                           a++;
-                      
-                       
-                    
-                   }
-                   temp1++;
-                   int temp2=0;
-                   //cout<<cnt<<" "<<b<<" "<<a;
-                   while(cnt==k&&b<a)
-                   {
-                       if(v[b]%2!=0)
-                       {
-                           cnt--;
-                       }
-                       else
-                           temp2++;
-                      // cout<<"shikhar";
-                       b++;
-                   }
-                   int preach=(temp1)+(temp1*temp2);
-                   shikhar+=preach;
-                   preach=0;
-                   a--;
-               }
-           }
-       
-            a++;
-           //cout<<a<<" "<<b<<" "<<cnt<<endl;
-          // if(flag)
-           //a++;
-           
-       }
-        return shikhar;
+            if(cnt==k)
+                ans++;
+            if(mp[cnt-k]>0)
+                ans+=mp[cnt-k];
+            mp[cnt]++;
+            right++;
+        }
+        return ans;
     }
 };
