44b17ddaa80d45b7e0da662e8dfbb63f99fd99e5
Shikhu416 <91547324+Shikhu416@users.noreply.github.com>
2022-09-24 09:16:55 +0530

    Time: 61 ms (9.62%), Space: 17.8 MB (93.60%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& v) {
-        int n=v.size();
-        sort(v.begin(),v.end());
-        bool flag=true;
-        for(int i=0;i<n;i++)
+        int n=v.size()-1,shikhar=0;
+       int sum=(n*(n+1))/2;
+        for(int i=0;i<=n;i++)
         {
-            if(v[i]!=i)
-            {
-                flag=false;
-                return i;
-                break;
-            }
+            shikhar+=v[i];
         }
-        
-            return n;
+        if(shikhar==sum)
+            return n+1;
+        else
+        {
+            return n+1-(shikhar-sum);
+        }
         
     }
 };
