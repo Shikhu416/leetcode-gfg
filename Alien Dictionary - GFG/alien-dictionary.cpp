050f39dd78268a1de5bc6a32e210d14b64e5ab62
Shikhu416 <91547324+Shikhu416@users.noreply.github.com>
2022-10-13 13:12:52 +0530

    Added solution - LeetHub

---
+++ Alien Dictionary - GFG/alien-dictionary.cpp
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+    string findOrder(string dict[], int n, int k) {
+        vector<int>adj[k];
+         vector<int>indegree(k,0);
+         for(int i=0;i<n-1;i++)
+         {
+             for(int j=0;j<min(dict[i].length(),dict[i+1].length());j++)
+             {
+                 if(dict[i][j]!=dict[i+1][j])
+                 {
+                       adj[dict[i][j]-'a'].push_back(dict[i+1][j]-'a');
+                      // cout<<dict[i+1][j]-'a'<<endl;
+                       indegree[dict[i+1][j]-'a']++;
+                       break;
+                 }
+                       
+             }
+         }
+         string shikhar="";
+        queue<int>q;
+        
+         for(int i=0;i<k;i++)
+         {
+             if(indegree[i]==0)
+               q.push(i);
+         }
+         while(!q.empty())
+         {
+             int temp=q.front();
+             q.pop();
+             shikhar+=(char)temp+97;
+             for(auto elem:adj[temp]){
+               indegree[elem]--;
+               if(indegree[elem]==0)
+                 q.push(elem);
+             }
+         }
+         //cout<<shikhar<<endl;
+         return shikhar;
+         
+    }
+};
+
+//{ Driver Code Starts.
+string order;
+bool f(string a, string b) {
+    int p1 = 0;
+    int p2 = 0;
+    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
+        p1 = order.find(a[i]);
+        p2 = order.find(b[i]);
+        //	cout<<p1<<" "<<p2<<endl;
+    }
+
+    if (p1 == p2 and a.size() != b.size()) return a.size() < b.size();
+
+    return p1 < p2;
+}
+
+// Driver program to test above functions
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, K;
+        cin >> N >> K;
+        string dict[N];
+        for (int i = 0; i < N; i++) cin >> dict[i];
+        
+        Solution obj;
+        string ans = obj.findOrder(dict, N, K);
+        order = "";
+        for (int i = 0; i < ans.size(); i++) order += ans[i];
+
+        string temp[N];
+        std::copy(dict, dict + N, temp);
+        sort(temp, temp + N, f);
+
+        bool f = true;
+        for (int i = 0; i < N; i++)
+            if (dict[i] != temp[i]) f = false;
+
+        if(f)cout << 1;
+        else cout << 0;
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
